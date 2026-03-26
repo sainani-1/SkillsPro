86095fadaf2792e65fd8b8358cd91ed7dca8c0fc
sainani-1 <2400030044@kluniversity.in>
2026-03-26 18:01:36 +0530

    updated new bug fixes for that balnk

--- src/main.jsx
+++ src/main.jsx
@@ -7,27 +7,6 @@ import { ChatProvider } from "./context/ChatContext";
 import { NotificationProvider } from "./context/NotificationContext";
 import GlobalInteractionGuards from "./components/GlobalInteractionGuards";
 
-const suppressConsoleOutput = () => {
-  const noop = () => {};
-  const methods = ["error", "warn", "log", "info", "debug"];
-
-  methods.forEach((method) => {
-    if (typeof console[method] === "function") {
-      console[method] = noop;
-    }
-  });
-
-  window.addEventListener("error", (event) => {
-    event.preventDefault();
-  });
-
-  window.addEventListener("unhandledrejection", (event) => {
-    event.preventDefault();
-  });
-};
-
-suppressConsoleOutput();
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
